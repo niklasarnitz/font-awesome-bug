a060aa6781af8ae5245aa669c69214bfa2825622
Niklas Arnitz <narnitz@churchtools.de>
2024-02-19 10:20:50 +0100

    added implementation of bug

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+library.add(fas, far);
 
 export default function App() {
+  const [iconToShow, setIconToShow] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FontAwesomeIcon
+        icon={iconToShow ? "bell" : "bell-slash"}
+        size={50}
+        color="black"
+      />
+      <TouchableOpacity
+        onPress={() => {
+          setIconToShow((prev) => !prev);
+        }}
+      >
+        <Text>Change icon</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
